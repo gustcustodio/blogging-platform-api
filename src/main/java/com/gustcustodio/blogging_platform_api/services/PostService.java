969a04ed811568c793fe49f74d1b1d681b150934
gustcustodio <gustcustodio@gmail.com>
2026-02-10 06:52:31 -0300

    create PostService with getSinglePost

---
+++ src/main/java/com/gustcustodio/blogging_platform_api/services/PostService.java
@@ -0,0 +1,29 @@
+package com.gustcustodio.blogging_platform_api.services;
+
+import com.gustcustodio.blogging_platform_api.dtos.PostDTO;
+import com.gustcustodio.blogging_platform_api.entities.Post;
+import com.gustcustodio.blogging_platform_api.mappers.PostMapper;
+import com.gustcustodio.blogging_platform_api.repositories.PostRepository;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+@Service
+public class PostService {
+
+    private PostRepository postRepository;
+
+    private PostMapper postMapper;
+
+    public PostService(PostRepository postRepository, PostMapper postMapper) {
+        this.postRepository = postRepository;
+        this.postMapper = postMapper;
+    }
+
+    @Transactional(readOnly = true)
+    public PostDTO getSinglePost(Long id) {
+        Post post = postRepository.findById(id).orElseThrow();
+        PostDTO postDTO = postMapper.convertEntityToDto(post);
+        return postDTO;
+    }
+
+}
