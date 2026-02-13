392df21718283ba71dd20813dc3d0d1f0931f330
gustcustodio <gustcustodio@gmail.com>
2026-02-13 06:01:06 -0300

    create DatabaseException

---
+++ src/main/java/com/gustcustodio/blogging_platform_api/services/exceptions/DatabaseException.java
@@ -0,0 +1,9 @@
+package com.gustcustodio.blogging_platform_api.services.exceptions;
+
+public class DatabaseException extends RuntimeException {
+
+    public DatabaseException(String message) {
+        super(message);
+    }
+
+}
